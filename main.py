0811b9e0797b829971c3988d79bdd8ff85bd65e3
Seho Lim <limseho657424@gmail.com>
2016-11-21 17:26:36 -0600

    fixed the render_str function

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 								autoescape = True)
 
-
 def render_str(template, **params):
 	t = jinja_env.get_template(template)
+	return t.render(params)
 
 def make_secure_val(val):
 	secured_value = hmac.new(secret, val).hexdigest()
